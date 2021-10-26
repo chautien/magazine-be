739bf81ebc2bc1f88bf7114fecc74903a3f2b65a
Tiến Châu V <tiencv@fiisoft.com>
2021-10-26 18:16:29 +0700

    feat: update api return getArticleBanner

--- app/controllers/controller.article.js
+++ app/controllers/controller.article.js
@@ -52,8 +52,7 @@ class ArticleController {
     try {
       const acticleBannerActive = await ArticleModel.find({
         banner_active: true,
-      });
-      console.log(acticleBannerActive);
+      }).select('title introduce thumbnail banner_active');
       res.status(200).json(acticleBannerActive);
     } catch (error) {
       res.status(502).json(error);
