216584152973957eabfbac1bfc2fc2273a346f23
Tiến Châu V <tiencv@fiisoft.com>
2021-11-05 09:02:06 +0700

    feat: update cors

--- app.js
+++ app.js
@@ -16,10 +16,7 @@ app.set('view engine', 'ejs');
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
+  res.header('Access-Control-Allow-Origin', 'true');
   next();
 });
 app.use(logger('dev'));
